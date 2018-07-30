42b0bf437280b502a7667e4c2225841d033c08e7
Juerg Beringer <juerg.beringer@gmail.com>
2018-07-29 21:26:33 -0700

    Add exmaple.py

---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.0'
+
+from cmdhelper import *
+
+cmdHelper = CmdHelper('argparse', __version__)
+cmdHelper.add_argument('cmd', help='command')
+cmdHelper.add_argument('args', help='command arguments', nargs='*')
+cmdHelper.add_option('-x', '--example', dest='value', default=None, help='sample option')
+options = cmdHelper.parse()
+
+try:
+    # processing goes here
+    run('python -c "print 2"',True)
+    pass    
+
+except Exception as e:
+     handleError(e,options.debug)
